1b3dcf6c5818471b69ed28410d0452749125431f
Maksym Lesyk <lesykmaksym@gmail.com>
2026-06-11 23:45:00 -0700

    Fix: fix sde url

--- static-data-service/src/main/java/org/mlesyk/staticdata/util/StaticDataFileLoader.java
+++ static-data-service/src/main/java/org/mlesyk/staticdata/util/StaticDataFileLoader.java
@@ -3,12 +3,16 @@ package org.mlesyk.staticdata.util;
 import org.mlesyk.staticdata.exception.ResourcesUrlFilePathParsingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.hash.HashCode;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.net.URL;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +39,12 @@ public class StaticDataFileLoader {
     private String eveResourcesURL;
 
     public URI getSDEFileURL() throws IOException, URISyntaxException {
-        return new URI(getAbsoluteFileUrlByFileName(eveResourcesURL, sdeFileName).attr(ABS_HREF));
+        JsonNode meta = new ObjectMapper().readTree(new URL(eveResourcesURL + "tranquility/latest.jsonl"));
+        JsonNode buildNode = meta.get("buildNumber");
+        if (buildNode == null || !buildNode.canConvertToLong()) {
+            throw new ResourcesUrlFilePathParsingException("Can't parse file URL");
+        }
+        return new URI(eveResourcesURL + "tranquility/eve-online-static-data-" + buildNode.asLong() + "-yaml.zip");
     }
 
     public String getRemoteSDEChecksum() throws IOException, URISyntaxException {
@@ -60,12 +69,6 @@ public class StaticDataFileLoader {
     public boolean sdeExists() {
         File file = new File(sdeFilePath, sdeFileName);
         return file.exists();
-    }
-
-    private Element getAbsoluteFileUrlByFileName(String resourcesURL, String fileName) throws IOException {
-        Document doc = Jsoup.connect(resourcesURL).get();
-        Elements links = doc.select("a");
-        return links.stream().filter(e -> e.attr(ABS_HREF).endsWith(fileName)).findFirst().orElseThrow(() -> new ResourcesUrlFilePathParsingException("Can't parse file URL"));
     }
 
     private Element getAbsoluteFileUrlByText(String resourcesURL, String urlText) throws IOException {
